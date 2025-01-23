de43f9273dd5b0cc3456bbbcc10ef499ee2bffec
femmebot <femmebot@gmail.com>
2025-01-23 17:31:03 -0500

    add base url to img src

--- src/components/experimental/ExifPhotoViewer.vue
+++ src/components/experimental/ExifPhotoViewer.vue
@@ -31,22 +31,22 @@ export default {
 				{ 
 					id: '000', 
 					title: 'Momo Bao', 
-					src: '/img/stamp/000.jpeg' 
+					src: '/vue-experiments/img/stamp/000.jpeg' 
 				},
 				{ 
 					id: '001', 
 					title: 'Ichiran Ramen', 
-					src: '/img/stamp/001.jpeg' 
+					src: '/vue-experiments/img/stamp/001.jpeg' 
 				},
 				{ 
 					id: '002', 
 					title: 'JR Fruit Park', 
-					src: '/img/stamp/002.jpeg' 
+					src: '/vue-experiments/img/stamp/002.jpeg' 
 				},
 				{ 
 					id: '004', 
 					title: 'Street Food Park', 
-					src: '/img/stamp/004.jpeg' 
+					src: '/vue-experiments/img/stamp/004.jpeg' 
 				},
 				// Add more images here
 			],
