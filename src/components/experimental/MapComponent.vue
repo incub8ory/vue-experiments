b34632d356fd564f75b096921889b8e05424562e
femmebot <femmebot@gmail.com>
2025-02-13 12:04:11 -0500

    add map marker images

--- src/components/experimental/MapComponent.vue
+++ src/components/experimental/MapComponent.vue
@@ -5,6 +5,7 @@
 <script>
 import { onMounted } from 'vue';
 import L from 'leaflet';
+import markerIcon from '../../../public/img/leaflet/marker-icon.png';
 
 export default {
 	name: 'MapComponent',
@@ -28,6 +29,11 @@ export default {
 	},
 	setup(props) {
 		onMounted(() => {
+			L.Marker.prototype.setIcon(
+				L.icon({
+					iconUrl: markerIcon,
+				})
+			);
 			const map = L.map(props.mapId).setView(
 				[props.latitude, props.longitude],
 				9
@@ -37,7 +43,7 @@ export default {
 			// stamen url="https://tiles.stadiamaps.com/tiles/stamen_toner/{z}/{x}/{y}{r}.png"
 			// leaflet url = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 			L.tileLayer(
-				'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+				'https://tiles.stadiamaps.com/tiles/stamen_toner/{z}/{x}/{y}{r}.png',
 				{
 					maxZoom: 20,
 				}
@@ -56,7 +62,7 @@ export default {
 	width: 100%;
 	/* height: 120px; */
 	/* opacity: 0.6; */
-  /* filter:sepia(100); */
+	/* filter:sepia(100); */
 }
 /* .map-container:hover {
 	opacity: 1;
